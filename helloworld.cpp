018ef0fb24e218f0e34337f84d0921f3689937be
Qian <583103067@qq.com>
2020-01-14 15:17:22 +0800

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,9 +1,11 @@
-#include<iostream>
-using namespace std;
-main()
+#include <iostream>
 
+using namespace std;
+
+int main()
 {
-   cout<<"hello world"<<endl;
-   
+   cout<<"hello world!"<<endl;
+   return 0;
 }
+
   
